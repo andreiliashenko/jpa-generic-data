e651202cd636ab6d781b5a321192402629431075
andreiliashenko <lyashenkoav92@gmail.com>
2015-08-11 22:29:37 +0400

    Timestamp converter method is added to abstract utility sql accessor

--- src/test/java/com/anli/generalization/data/utils/SqlHelper.java
+++ src/test/java/com/anli/generalization/data/utils/SqlHelper.java
@@ -6,6 +6,7 @@ import com.anli.sqlexecution.handling.TransformingResultSet;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.LinkedList;
 import java.util.List;
 import javax.sql.DataSource;
@@ -25,13 +26,17 @@ public class SqlHelper {
     protected Integer getInteger(BigDecimal bigDecimal) {
         return bigDecimal != null ? bigDecimal.intValueExact() : null;
     }
-    
+
+    protected BigInteger getTime(Timestamp timestamp) {
+        return timestamp != null ? BigInteger.valueOf(timestamp.getTime()) : null;
+    }
+
     protected class IdSelector implements ResultSetHandler<List<BigInteger>> {
 
         @Override
         public List<BigInteger> handle(TransformingResultSet resultSet) throws SQLException {
             List<BigInteger> ids = new LinkedList<>();
-            while(resultSet.next()) {
+            while (resultSet.next()) {
                 ids.add(getBigInteger(resultSet.getValue(1, BigDecimal.class)));
             }
             return ids;
