6d23ed0d3c6bdd161fd47a71b8e77077a8e90e10
andreiliashenko <lyashenkoav92@gmail.com>
2015-08-11 22:28:12 +0400

    Alias method for time construction is added to test value factory

--- src/test/java/com/anli/generalization/data/utils/ValueFactory.java
+++ src/test/java/com/anli/generalization/data/utils/ValueFactory.java
@@ -1,10 +1,16 @@
 package com.anli.generalization.data.utils;
 
 import java.math.BigInteger;
+import org.joda.time.DateTime;
 
 public class ValueFactory {
 
     public static BigInteger bi(long value) {
         return BigInteger.valueOf(value);
+    }
+
+    public static DateTime dt(int year, int month, int day, int hours, int minutes, int seconds,
+            int milliseconds) {
+        return new DateTime(year, month, day, hours, minutes, seconds, milliseconds);
     }
 }
